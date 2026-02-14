a8985f2e8bc5d3769e8842fa3082e898e19e7d45
660710232 <phantawong_p@silpakorn.edu>
2026-02-14 13:55:03 +0700

    datatype

---
+++ datatype.go
@@ -0,0 +1,31 @@
+package main
+import ("fmt")
+
+func datatype() {
+	// type of variable
+	// Go has several data types such as int, float, string, bool, etc.
+	// like bool, Numeric types (int, float, etc.), string, array, slice, map, struct, pointer, etc.
+	var number int = 10
+	var pi float64 = 3.14
+	var name string = "John"
+	var isTrue bool = true
+	fmt.Println(number)
+	fmt.Println(pi)
+	fmt.Println(name)
+	fmt.Println(isTrue)
+
+	// boolean is unique data type that can only have two values: true or false
+	
+	// Interger is  a number like 1,2,3,4,5, etc. it can be positive or negative
+	// unsigned integer is a only positive integer and signed integer is a positive and negative
+	// int use 32 bit and 64 bit (system)
+	// int have int8, int16, int32, int64 
+	// like uint8, uint16, uint32, uint64
+
+	// float is a number with decimal point like 3.14, 2.718, etc.
+	// float have float32 and float64
+
+	// string is a sequence of characters
+
+
+}
