ab84717332e96aaadbb900662c811a63a183ad52
660710232 <phantawong_p@silpakorn.edu>
2026-02-14 13:34:45 +0700

    output print

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ func main() {
 	variable()
 	naming()
 	constant()
+	output()
 }
---
+++ output.go
@@ -0,0 +1,23 @@
+package main
+import ("fmt")
+
+func output() {
+	// Output in golang is done using fmt package
+	// Print is used to print without new line
+	var hello string = "Hello"
+	var world string = "World"
+	fmt.Print(hello) // Hello
+	fmt.Print(world) // World
+	// it should be in one line because we use Print
+
+	// Println is used to print with new line
+	fmt.Println(hello) // Hello
+	fmt.Println(world) // World
+	// it should be in two line because we use Println
+
+	// Printf is used to print with format
+	// %v is used to print value
+	// %T is used to print type
+	fmt.Printf("Value: %v, Type: %T\n", hello, hello) // Value: Hello, Type: string
+
+}
